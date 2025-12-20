1fa7522a695354b37d87d5475115e5bc892a32ec
Tatsuma Kawanishi <156042230+K-T-classical@users.noreply.github.com>
2025-12-20 14:55:29 +0900

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CalculusWorkbook from './workbook_result_calculus';
+
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-slate-50">
+      <CalculusWorkbook />
+    </div>
+  )
+}
+
+export default App
